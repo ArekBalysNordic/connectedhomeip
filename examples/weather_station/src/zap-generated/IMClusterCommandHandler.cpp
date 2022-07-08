57a72e20bd0c05be0c225e3d92a5f68d8e214c21
Damian Krolik <damian.krolik@nordicsemi.no>
2022-07-08 13:54:05 +0200

    Regen ZAP

--- examples/weather_station/src/zap-generated/IMClusterCommandHandler.cpp
+++ examples/weather_station/src/zap-generated/IMClusterCommandHandler.cpp
@@ -170,6 +170,50 @@ namespace app
 
 		} // namespace GeneralCommissioning
 
+		namespace GeneralDiagnostics
+		{
+			void DispatchServerCommand(CommandHandler *apCommandObj,
+						   const ConcreteCommandPath &aCommandPath, TLV::TLVReader &aDataTlv)
+			{
+				CHIP_ERROR TLVError = CHIP_NO_ERROR;
+				bool wasHandled = false;
+				{
+					switch (aCommandPath.mCommandId) {
+					case Commands::TestEventTrigger::Id: {
+						Commands::TestEventTrigger::DecodableType commandData;
+						TLVError = DataModel::Decode(aDataTlv, commandData);
+						if (TLVError == CHIP_NO_ERROR) {
+							wasHandled =
+								emberAfGeneralDiagnosticsClusterTestEventTriggerCallback(
+									apCommandObj, aCommandPath, commandData);
+						}
+						break;
+					}
+					default: {
+						// Unrecognized command ID, error status will apply.
+						apCommandObj->AddStatus(
+							aCommandPath,
+							Protocols::InteractionModel::Status::UnsupportedCommand);
+						ChipLogError(Zcl,
+							     "Unknown command " ChipLogFormatMEI
+							     " for cluster " ChipLogFormatMEI,
+							     ChipLogValueMEI(aCommandPath.mCommandId),
+							     ChipLogValueMEI(aCommandPath.mClusterId));
+						return;
+					}
+					}
+				}
+
+				if (CHIP_NO_ERROR != TLVError || !wasHandled) {
+					apCommandObj->AddStatus(aCommandPath,
+								Protocols::InteractionModel::Status::InvalidCommand);
+					ChipLogProgress(Zcl, "Failed to dispatch command, TLVError=%" CHIP_ERROR_FORMAT,
+							TLVError.Format());
+				}
+			}
+
+		} // namespace GeneralDiagnostics
+
 		namespace GroupKeyManagement
 		{
 			void DispatchServerCommand(CommandHandler *apCommandObj,
@@ -585,6 +629,9 @@ namespace app
 			break;
 		case Clusters::GeneralCommissioning::Id:
 			Clusters::GeneralCommissioning::DispatchServerCommand(apCommandObj, aCommandPath, aReader);
+			break;
+		case Clusters::GeneralDiagnostics::Id:
+			Clusters::GeneralDiagnostics::DispatchServerCommand(apCommandObj, aCommandPath, aReader);
 			break;
 		case Clusters::GroupKeyManagement::Id:
 			Clusters::GroupKeyManagement::DispatchServerCommand(apCommandObj, aCommandPath, aReader);
