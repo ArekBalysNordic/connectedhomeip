9888b004a77b5b432bec87e2299334e2c871eed8
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2022-11-15 12:25:26 +0100

    Aligned Lighting app to NCS and added WiFi support

--- examples/lighting-app/nrfconnect/main/main.cpp
+++ examples/lighting-app/nrfconnect/main/main.cpp
@@ -73,7 +73,7 @@ int main()
 
     if (err == CHIP_NO_ERROR)
     {
-        err = GetAppTask().StartApp();
+        err = AppTask::Instance().StartApp();
     }
 
     LOG_ERR("Exited with code %" CHIP_ERROR_FORMAT, err.Format());
