905d008e52e1a1f8a7fcf5d9ee4a784ceb89f95a
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2023-02-14 07:56:07 +0100

    [dnssd] Parameterize min and max lookup times
    In some cases when devices are located at a close distance the minimum
    lookup time is not small enough to send a DNSSD query.
    
    This change allows parametrizing the max and min lookup times
    and changing it according to platform requirements.

--- src/lib/address_resolve/AddressResolve.h
+++ src/lib/address_resolve/AddressResolve.h
@@ -19,6 +19,7 @@
 #include <lib/core/PeerId.h>
 #include <lib/support/IntrusiveList.h>
 #include <messaging/ReliableMessageProtocolConfig.h>
+#include <platform/CHIPDeviceConfig.h>
 #include <system/SystemClock.h>
 #include <system/SystemLayer.h>
 #include <transport/raw/PeerAddress.h>
@@ -141,8 +142,8 @@ public:
     }
 
 private:
-    static constexpr uint32_t kMinLookupTimeMsDefault = 200;
-    static constexpr uint32_t kMaxLookupTimeMsDefault = 45000;
+    static constexpr uint32_t kMinLookupTimeMsDefault = CHIP_CONFIG_ADDRESS_RESOLVE_MIN_LOOKUP_DEFAULT_TIME_MS;
+    static constexpr uint32_t kMaxLookupTimeMsDefault = CHIP_CONFIG_ADDRESS_RESOLVE_MAX_LOOKUP_DEFAULT_TIME_MS;
 
     PeerId mPeerId;
     System::Clock::Milliseconds32 mMinLookupTimeMs{ kMinLookupTimeMsDefault };
